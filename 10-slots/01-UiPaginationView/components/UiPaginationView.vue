6acd1f9a58ecd7eb4aaa9d35ad547f5090850ce0
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-03-21 23:08:53 +0300

    Added solution for task: UiPaginationView

--- 10-slots/01-UiPaginationView/components/UiPaginationView.vue
+++ 10-slots/01-UiPaginationView/components/UiPaginationView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="pagination-container">
-    <!-- Контент страницы -->
+    <slot v-for="item in pageItems" :item="item" />
   </div>
 </template>
 
@@ -14,14 +14,24 @@ export default {
       required: true,
       validator: (value) => value > 0,
     },
+
     perPage: {
       type: Number,
       required: true,
       validator: (value) => value > 0,
     },
+
     items: {
       type: Array,
       required: true,
+    },
+  },
+
+  computed: {
+    pageItems() {
+      const start = (this.page - 1) * this.perPage;
+      const end = this.page * this.perPage;
+      return this.items.slice(start, end);
     },
   },
 };
